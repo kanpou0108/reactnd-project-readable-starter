cfac67868c3848ac178b7fa6a58adbc4a8780c34
Junpei Ihara <seapig.dolphin2925@gmail.com>
2017-12-17 02:23:36 +0900

    fixing

--- frontend/src/redux/selectors/posts.js
+++ frontend/src/redux/selectors/posts.js
@@ -3,14 +3,21 @@ import { createSelector } from 'reselect';
 const getPostIdsByCategory = (state, category) =>
   (state.postsByCategory[category] ? state.postsByCategory[category].ids : []);
 const getPostObjects = state => state.entities.posts;
+
 export const getPost = (state, postId) => state.entities.posts[postId];
+
 export const getIsFetching = (state, category) =>
   (state.postsByCategory[category] ? state.postsByCategory[category].isFetching : true);
+
 export const getErrorMessage = (state, category) =>
   (state.postsByCategory[category] ? state.postsByCategory[category].errorMessage : null);
+
 export const getPostVoteScore = (state, postId) => state.entities.posts[postId].voteScore;
+
 export const getPostCategory = (state, postId) => state.entities.posts[postId].category;
+
 export const getPostComments = (state, postId) => state.entities.posts[postId].comments;
+
 export const getShouldFetch = (state, category) =>
   (state.postsByCategory[category]
     ? state.postsByCategory[category].shouldFetch
