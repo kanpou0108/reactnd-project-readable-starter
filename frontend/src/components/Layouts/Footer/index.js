2e0ba53b843326abdc967e3f1ab13354ef898a93
Junpei Ihara <seapig.dolphin2925@gmail.com>
2017-12-17 14:04:38 +0900

    fixing

--- frontend/src/components/Layouts/Footer/index.js
+++ frontend/src/components/Layouts/Footer/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { FaGithub } from 'react-icons/lib/fa';
 
 import styles from './styles.css';
 
 const Footer = () => (
   <footer className={styles.appFooter}>
     <span>Readable Project - Udacity React NanoDegree course<br />
-      © 2017 Jan Vigar All Rights Reserved<br />
-      <a href="https://github.com/jansvigar/reactnd-project-readable"><FaGithub /></a>
     </span>
   </footer>
 );
